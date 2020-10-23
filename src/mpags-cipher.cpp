2a6ffdb0fcb13954b5e299c301644f06fd13a572
Jack Matthews <jpm602@student.bham.ac.uk>
2020-10-23 11:18:08 +0100

    exercise on function arguments

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -63,15 +63,71 @@ std::string transformChar(const char in_char)
   return "";
 }
 
+// Command Line argument processing
+bool processCommandLine(
+  const std::vector<std::string>& args,
+  bool& helpRequested,
+  bool& versionRequested,
+  std::string& inputFileName,
+  std::string& outputFileName)
+  {
+      // Add a typedef that assigns another name for the given type for clarity
+    typedef std::vector<std::string>::size_type size_type;
+    const size_type nCmdLineArgs {args.size()};
+
+    // Process command line arguments - ignore zeroth element, as we know this to
+    // be the program name and don't need to worry about it
+    for (size_type i {1}; i < nCmdLineArgs; ++i) {
+
+      if (args[i] == "-h" || args[i] == "--help") {
+        helpRequested = true;
+      }
+      else if (args[i] == "--version") {
+        versionRequested = true;
+      }
+      else if (args[i] == "-i") {
+        // Handle input file option
+        // Next element is filename unless -i is the last argument
+        if (i == nCmdLineArgs-1) {
+    std::cerr << "[error] -i requires a filename argument" << std::endl;
+    // exit main with non-zero return to indicate failure
+    return false;
+        }
+        else {
+    // Got filename, so assign value and advance past it
+    inputFileName = args[i+1];
+    ++i;
+        }
+      }
+      else if (args[i] == "-o") {
+        // Handle output file option
+        // Next element is filename unless -o is the last argument
+        if (i == nCmdLineArgs-1) {
+    std::cerr << "[error] -o requires a filename argument" << std::endl;
+    // exit main with non-zero return to indicate failure
+    return false;
+        }
+        else {
+    // Got filename, so assign value and advance past it
+    outputFileName = args[i+1];
+    ++i;
+        }
+      }
+      else {
+        // Have an unknown flag to output error message and return non-zero
+        // exit status to indicate failure
+        std::cerr << "[error] unknown argument '" << args[i] << "'\n";
+        return false;
+      }
+    }
+    return true;
+  }
+
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
 {
   // Convert the command-line arguments into a more easily usable form
   const std::vector<std::string> cmdLineArgs {argv, argv+argc};
-
-  // Add a typedef that assigns another name for the given type for clarity
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {cmdLineArgs.size()};
 
   // Options that might be set by the command-line arguments
   bool helpRequested {false};
@@ -79,50 +135,14 @@ int main(int argc, char* argv[])
   std::string inputFile {""};
   std::string outputFile {""};
 
-  // Process command line arguments - ignore zeroth element, as we know this to
-  // be the program name and don't need to worry about it
-  for (size_type i {1}; i < nCmdLineArgs; ++i) {
+  // Process command line arguments
+  bool argsWorked = processCommandLine(cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile);
 
-    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-      helpRequested = true;
-    }
-    else if (cmdLineArgs[i] == "--version") {
-      versionRequested = true;
-    }
-    else if (cmdLineArgs[i] == "-i") {
-      // Handle input file option
-      // Next element is filename unless -i is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -i requires a filename argument" << std::endl;
-	// exit main with non-zero return to indicate failure
-	return 1;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	inputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else if (cmdLineArgs[i] == "-o") {
-      // Handle output file option
-      // Next element is filename unless -o is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -o requires a filename argument" << std::endl;
-	// exit main with non-zero return to indicate failure
-	return 1;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	outputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else {
-      // Have an unknown flag to output error message and return non-zero
-      // exit status to indicate failure
-      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
-      return 1;
-    }
+  // Error if arguments didn't work
+  if(!argsWorked)
+  {
+    std::cerr << "Argument processing failed" << std::endl;
+    return 1;
   }
 
   // Handle help, if requested
